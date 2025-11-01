1adeb945c74d234ead160d7613f00204b4d99569
nguyenchicong-catdang <nguyenchicong.catdang@gmail.com>
2025-11-01 16:05:36 +0700

    up handles vite_plugin_rewrite_all -> lap qua mang

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,8 +1,13 @@
 // vite/vite.config.js
-import { defineConfig } from 'vite'
-import { vite_include_html_plugin } from './vite-plugins-handles/vite-include-html-plugin/vite-include-html-plugin'
+import { defineConfig } from 'vite';
+import { vite_include_html_plugin } from './vite-plugins-handles/vite-include-html-plugin/vite-include-html-plugin';
+import { vite_plugin_rewrite_all } from './vite-plugins-handles/vite-plugin-rewrite-all/vite-plugin-rewrite-all';
 export default defineConfig({
   // ...
   root: 'vite',
-  plugins: [vite_include_html_plugin()]
+  plugins: [
+    vite_include_html_plugin(),
+    // @param: string []
+    vite_plugin_rewrite_all(['post'])
+  ]
 })
